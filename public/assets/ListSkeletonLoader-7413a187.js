3731e129393877e7e1f0cddca9a100b9ae06abde
Diego Mc <projkd1@gmail.com>
2023-02-20 20:51:45 +0200

    build

---
+++ public/assets/ListSkeletonLoader-7413a187.js
@@ -0,0 +1,1 @@
+import{b as e,j as s}from"./index-5007061c.js";const a=({})=>e("div",{className:"list-skeleton",children:[s("div",{className:"skeleton-box"}),s("div",{className:"skeleton-box"}),s("div",{className:"skeleton-box"}),s("div",{className:"skeleton-box"}),s("div",{className:"skeleton-box"})]});export{a as L};
